24769005dd2e6b83019bd3eccb7976b707ab4388
Jochen Alt <jochen.alt@gmx.net>
2016-08-23 14:46:03 +0200

    started documentation

--- code/BotMainBoard/src/setup.cpp
+++ code/BotMainBoard/src/setup.cpp
@@ -13,7 +13,7 @@
 ActuatorLimitsType actuatorLimits =  {
 	{ radians(-179.0f)	,radians(179.0f)},  // Hip
 	{ radians(-89.0f)	,radians(89.0f)},	// Upperarm
-	{ radians(-240.0f)	,radians(45.0f)},	// Forearm
+	{ radians(-230.0f)	,radians(45.0f)},	// Forearm
 	{ radians(-180.0f)	,radians(180.0f)}, 	// Ellbow
 	{ radians(-100.0f)	,radians(100.0f)}, 	// Wrist
 	{ radians(-180.0f)	,radians(180.0f)}, 	// Hand
