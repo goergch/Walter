56d1051ce1ea05c2485091945216c638dc5f7931
Jochen Alt <jochen.alt@gmx.net>
2016-09-18 02:06:10 +0200

    adaped acceleration of hip

--- code/ActuatorController/ActuatorController/ActuatorController/Config.cpp
+++ code/ActuatorController/ActuatorController/ActuatorController/Config.cpp
@@ -165,8 +165,8 @@ StepperSetupData stepperSetup[MAX_STEPPERS] {
 	{ WRIST,    true,     8,         PIN_A1, PIN_A2, PIN_A3, 1.8,  /*(56.0/16.0),			  160 ,     800,*/   0.4, BLACK, RED, BLUE, GREEN},
 	{ ELLBOW,   true,     4,         PIN_A4, PIN_A5, PIN_A6, 1.8,  /*(56.0/16.0)*(22.0/16.0), 160 ,     600,*/   0.4, BLACK, GREEN, RED, BLUE},
 	{ FOREARM,  true,     16,        PIN_A7, PIN_C7, PIN_C6, 1.8,  /*(60.0/14.0)*(48.0/18.0), 200 ,     600,*/   1.4, NON_COLOR, NON_COLOR, NON_COLOR, NON_COLOR},
-	{ UPPERARM, true,     16,         PIN_C5, PIN_C4, PIN_C3, 1.8,  /*(80.0/14.0)*(48.0/18.0), 160 ,     600,*/   3.5, BLACK, RED, BLUE, GREEN},
-	{ HIP,      false,    16,         PIN_C2, PIN_D7, PIN_D6, 1.8,  /*(90.0/12.0),             160 ,     600,*/   2.8, BLACK, GREEN, RED, BLUE} };
+	{ UPPERARM, true,     16,        PIN_C5, PIN_C4, PIN_C3, 1.8,  /*(80.0/14.0)*(48.0/18.0), 160 ,     600,*/   3.5, BLACK, GREEN, RED, BLUE},
+	{ HIP,      false,    16,        PIN_C2, PIN_D7, PIN_D6, 1.8,  /*(90.0/12.0),             160 ,     600,*/   2.8, BLACK, GREEN, RED, BLUE} };
 
 RotaryEncoderSetupData encoderSetup[MAX_ENCODERS] {
 	// 	ActuatorId/ programmI2CAddress / I2CAddreess / clockwise
--- code/Core/src/ActuatorProperty.cpp
+++ code/Core/src/ActuatorProperty.cpp
@@ -13,11 +13,11 @@
 // configuration data of joints
 // (gear ratio, min[rad], max[rad], maxSpeed[RPM], maxAcc [RPM/s*s]
 AllActuatorsConfigType actuatorConfigType =  {
-	{ (90.0/12.0), 				radians(-179.0f)	,radians(179.0f), 	160, 400}, // Hip
-	{ (80.0/12.0)*(48.0/14.0), 	radians(-89.0f)		,radians(89.0f), 	160, 1201}, // Upperarm
-	{ (60.0/14.0)*(48.0/18.0), 	radians(-230.0f)	,radians(45.0f), 	200, 1202}, // Forearm
-	{ (56.0/16.0)*(22.0/16.0), 	radians(-180.0f)	,radians(180.0f),	160, 1203}, // Ellbow
-	{ 1.0, 						radians(-100.0f)	,radians(100.0f),	160, 1204}, // Wrist
-	{ 1.0, 						radians(-180.0f)	,radians(180.0f),	160, 1205}, // Hand
-	{ 1.0, 						radians(11.0f)		,radians(60.0f),	160, 1206}  // Gripper
+	{ (90.0/12.0), 				radians(-179.0f)	,radians(179.0f), 	160, 400},  // Hip
+	{ (80.0/12.0)*(48.0/14.0), 	radians(-89.0f)		,radians(89.0f), 	160, 600}, // Upperarm
+	{ (60.0/14.0)*(48.0/18.0), 	radians(-230.0f)	,radians(45.0f), 	200, 1200}, // Forearm
+	{ (56.0/16.0)*(22.0/16.0), 	radians(-180.0f)	,radians(180.0f),	160, 1200}, // Ellbow
+	{ 1.0, 						radians(-100.0f)	,radians(100.0f),	160, 1200}, // Wrist
+	{ 1.0, 						radians(-180.0f)	,radians(180.0f),	160, 1200}, // Hand
+	{ 1.0, 						radians(11.0f)		,radians(60.0f),	160, 1200}  // Gripper
 };
