9e5fc6cafec1fcafae0080ee2907e6ff48ea3b17
Jochen Alt <jochen.alt@gmx.net>
2016-12-18 15:39:48 +0100

    teensy knob works

--- code/BotCortex/config.h
+++ code/BotCortex/config.h
@@ -1,3 +1,45 @@
 
 
+#define LED_PIN 13
+
 #define CONNECTION_BAUD_RATE 115200			// baud rate for connection to main board
+#define PIBOT_PULSE_WIDTH_US 1				// pulse width for PiBot drivers
+#define PIN_RX1 0
+#define PIN_TX1 1
+
+#define PIN_RX2 9
+#define PIN_TX2 10
+
+#define PIN_RX3 7
+#define PIN_TX3 8
+
+#define PIN_SDA0 18
+#define PIN_SCL0 19
+#define PIN_SDA1 38
+#define PIN_SCL1 37
+
+#define KNOB_PIN 15
+
+/* Command line via Serial1 */
+#define SERIAL_CMD_RX PIN_RX1
+#define SERIAL_CMD_TX PIN_TX1
+
+/* logging output via Serial2 (actually only TX is needed) */
+#define SERIAL_LOG_RX PIN_RX2
+#define SERIAL_LOG_TX PIN_TX2
+
+/* Herkulex Servos are connected via Serial3 */
+#define HERKULEX_RX PIN_RX3
+#define HERKULEX_TX PIN_TX3
+
+#define SENSOR0_SDA PIN_SDA0
+#define SENSOR0_SCL PIN_SCL0
+#define SENSOR1_SDA PIN_SDA1
+#define SENSOR1_SCL PIN_SCL1
+
+#define SERIAL_LOG_RX PIN_RX2
+#define SERIAL_LOG_TX PIN_TX2
+
+#define STEPPER0_EN_PIN 	2
+#define STEPPER0_DIR_PIN 	3
+#define STEPPER0_CLK_PIN 	5
--- code/BotCortex/main.cpp
+++ code/BotCortex/main.cpp
@@ -4,28 +4,76 @@
 #include "PatternBlinker.h"
 #include <AccelStepper.h>
 
+static uint8_t IdlePattern[2] = { 0b10000000, 0b00000000, };		// boring
+static uint8_t DefaultPattern[3] = { 0b11001000, 0b00001100, 0b10000000 };// nice!
+static uint8_t LEDOnPattern[1] = { 0b11111111 };
+static uint8_t LEDOffPattern[1] = { 0b00000000 };
 
-int LED_PIN = 13;
-
-static uint8_t IdlePattern[2]  =   { 0b10000000,0b00000000,};				 // boring
-static uint8_t DefaultPattern[3] = { 0b11001000,0b00001100,0b10000000};	     // nice!
-static uint8_t LEDOnPattern[1]  =  { 0b11111111 };
-static uint8_t LEDOffPattern[1] =  { 0b00000000 };
-
-PatternBlinker ledBlinker(LED_PIN,100);
+PatternBlinker ledBlinker(LED_PIN, 100);
+TimePassedBy 	motorKnobTimer;		// used for measuring sample rate of motor knob
 
 HardwareSerial* cmdSerial = &Serial1;
-HardwareSerial* logSerial = &Serial2;
-
+HardwareSerial* logSerial = &Serial1;
 
 AccelStepper stepper;
-
+float motorAngle  = 0;
 void forward(void* obj) {
+	digitalWrite(STEPPER0_DIR_PIN, HIGH);
 
+	digitalWrite(STEPPER0_CLK_PIN, LOW);
+	delayMicroseconds(PIBOT_PULSE_WIDTH_US);
+	digitalWrite(STEPPER0_CLK_PIN, HIGH);
 }
+
 void backward(void* obj) {
+	digitalWrite(STEPPER0_DIR_PIN, LOW);
 
+	digitalWrite(STEPPER0_CLK_PIN, LOW);
+	delayMicroseconds(PIBOT_PULSE_WIDTH_US);
+	digitalWrite(STEPPER0_CLK_PIN, HIGH);
 }
+
+void logPinAssignment() {
+	logSerial->println("PIN assignment");
+	logSerial->print("knob           = ");
+	logSerial->println(KNOB_PIN);
+	logSerial->print("Cmd     RX,TX  = (");
+	logSerial->print(SERIAL_CMD_RX);
+	logSerial->print(",");
+	logSerial->print(SERIAL_CMD_RX);
+	logSerial->println(")");
+	logSerial->print("Logger  RX,TX  = (");
+	logSerial->print(SERIAL_LOG_RX);
+	logSerial->print(",");
+	logSerial->print(SERIAL_LOG_RX);
+	logSerial->println(")");
+	logSerial->print("Herkulex RX,TX  = (");
+	logSerial->print(HERKULEX_RX);
+	logSerial->print(",");
+	logSerial->print(HERKULEX_TX);
+	logSerial->println(")");
+
+	logSerial->print("Sensor0  SCl,SDA = (");
+	logSerial->print(SENSOR0_SCL);
+	logSerial->print(",");
+	logSerial->print(SENSOR0_SDA);
+	logSerial->println(")");
+
+	logSerial->print("Sensor1  SCL,SDA = (");
+	logSerial->print(SENSOR1_SCL);
+	logSerial->print(",");
+	logSerial->print(SENSOR1_SDA);
+	logSerial->println(")");
+
+	logSerial->print("Stepper0 En,Dir,CLK = (");
+	logSerial->print(STEPPER0_EN_PIN);
+	logSerial->print(",");
+	logSerial->print(STEPPER0_DIR_PIN);
+	logSerial->print(",");
+	logSerial->print(STEPPER0_CLK_PIN);
+	logSerial->println(")");
+}
+
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -33,7 +81,7 @@ void setup() {
 	wdt_enable(WDTO_2S);
 
 	// nice blinking pattern
-	ledBlinker.set(DefaultPattern,sizeof(DefaultPattern));
+	ledBlinker.set(DefaultPattern, sizeof(DefaultPattern));
 
 	// establish serial output and say hello
 	cmdSerial->begin(CONNECTION_BAUD_RATE);
@@ -43,11 +91,78 @@ void setup() {
 	logSerial->begin(CONNECTION_BAUD_RATE);
 	logSerial->println("--- WALTER's logging ---");
 
-	stepper.setup(NULL,forward,backward);
-	stepper.setMaxSpeed(1000);
+	logPinAssignment();
+
+	pinMode(STEPPER0_CLK_PIN, OUTPUT);
+	pinMode(STEPPER0_EN_PIN, OUTPUT);
+	pinMode(STEPPER0_DIR_PIN, OUTPUT);
+	pinMode(4, OUTPUT);
+
+	digitalWrite(STEPPER0_CLK_PIN, HIGH);
+	digitalWrite(STEPPER0_DIR_PIN, LOW);
+	digitalWrite(STEPPER0_EN_PIN, LOW);
+
+	stepper.setup(NULL, forward, backward);
+	stepper.setMaxSpeed(1000000);
+	stepper.setAcceleration(100000);
+
+	int adcValue = analogRead(KNOB_PIN);
+	float angle = (float(adcValue - 512) / 512.0) * (270.0 / 2.0);
+	motorAngle = angle;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+	stepper.run();
+
 	ledBlinker.loop(millis());	// blink
+
+	if (motorKnobTimer.isDue_ms(100, millis())) {
+		int adcValue = analogRead(KNOB_PIN);
+		float angle = (float(adcValue - 512) / 512.0) * (270.0 / 2.0);
+		float angleDiff = angle - motorAngle;
+		const float gearbox = 65.0/15.0;
+		const float degreePerStep = 1.8;
+		const float microsteps = 16;
+		const float degreePerMicrostep = degreePerStep/microsteps;
+
+		int steps = gearbox * angleDiff / degreePerMicrostep ;
+		logSerial->print("angle=");
+		logSerial->print(angle);
+		logSerial->print("motor=");
+		logSerial->print(motorAngle);
+		logSerial->print("diff=");
+		logSerial->print(angleDiff);
+		logSerial->print("steps=");
+		logSerial->print(steps);
+		logSerial->println();
+
+		stepper.move(steps);
+		motorAngle += angleDiff;
+
+
+		digitalWrite(STEPPER0_EN_PIN, HIGH);
+/*
+		static bool b = true;
+		if (b) {
+			digitalWrite(STEPPER0_EN_PIN, LOW);
+			b = false;
+		}
+		else {
+			digitalWrite(STEPPER0_EN_PIN, HIGH);
+			b = true;
+		}
+*/
+	}
+
+	/*
+	if (fabs(angle - lastAngle) > 2.0) {
+		logSerial->print("knob=");
+		logSerial->println(angle);
+
+		// stepper.move((360.0/degreePerStep) * gearbox * microsteps * (angle-lastAngle));
+		lastAngle = angle;
+
+	}
+	*/
 }
