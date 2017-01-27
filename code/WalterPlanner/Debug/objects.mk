36253cc6e4d59c18c495667cc5b0d02d3b89418f
Jochen Alt <jochen.alt@gmx.net>
2017-01-27 09:58:58 +0100

    eclipse project WalterPlanner re-setup

--- code/WalterPlanner/Debug/objects.mk
+++ code/WalterPlanner/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lWalterKinematics -lWalterCommon -lfreeglut_static -lwinmm -lcomdlg32 -lole32 -loleaut32 -lGLUI -lglu32 -lopengl32 -lgdi32 -lPocoNetd -lPocoFoundationd -lCppUnitd -lws2_32 -lIphlpapi
+LIBS := -lWalterCommon -lWalterKinematics -lfreeglut_static -lwinmm -lcomdlg32 -lole32 -loleaut32 -lGLUI -lglu32 -lopengl32 -lgdi32 -lPocoNetd -lPocoFoundationd -lCppUnitd -lws2_32 -lIphlpapi
 
---
+++ code/WalterPlanner/Debug/src/subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/BotDrawer.cpp \
+../src/BotView.cpp \
+../src/BotWindowCtrl.cpp \
+../src/CADSTLObject.cpp \
+../src/ExecutionInvoker.cpp \
+../src/TrajectorySimulation.cpp \
+../src/TrajectoryView.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/BotDrawer.o \
+./src/BotView.o \
+./src/BotWindowCtrl.o \
+./src/CADSTLObject.o \
+./src/ExecutionInvoker.o \
+./src/TrajectorySimulation.o \
+./src/TrajectoryView.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/BotDrawer.d \
+./src/BotView.d \
+./src/BotWindowCtrl.d \
+./src/CADSTLObject.d \
+./src/ExecutionInvoker.d \
+./src/TrajectorySimulation.d \
+./src/TrajectoryView.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DFREEGLUT_STATIC -DPOCO_STATIC -I"E:\Projects\Arm\code\WalterPlanner\src" -I"E:\Projects\Arm\code\WalterCommon\src" -I"E:\Projects\Arm\code\WalterKinematics\src" -I"E:\Projects\Arm\code\GLUI\src\include" -I"E:\Projects\Arm\code\poco-1.7.6\Net\include" -I"E:\Projects\Arm\code\poco-1.7.6\Foundation\include" -I"E:\Projects\Arm\code\freeglut\include" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
