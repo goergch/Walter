449e27f91712e299a14d6d760a956ba1b7a89eed
Jochen Alt <jochen.alt@gmx.net>
2017-02-19 00:05:52 +0100

    Towers of Hanoi now consider disk diameter

--- code/WalterPlanner/src/Hanoi.cpp
+++ code/WalterPlanner/src/Hanoi.cpp
@@ -18,36 +18,54 @@ void Hanoi::towers(int NumberOfDisks, int frompeg, int topeg, int auxpeg)
 {
     if (NumberOfDisks == 1)
     {
-    	move(1, frompeg, topeg);
+    	move(frompeg, topeg);
         LOG(DEBUG) << "Move disk 1 from peg "<<frompeg<<" to peg "<<topeg;
         return;
     }
 
 	towers(NumberOfDisks - 1, frompeg, auxpeg, topeg);
 
-	move(NumberOfDisks, frompeg, topeg);
+	move(frompeg, topeg);
 	LOG(DEBUG) << "Move disk "<< NumberOfDisks <<" from peg "<<frompeg<<" to peg "<<topeg;
 
     towers(NumberOfDisks - 1, auxpeg, topeg, frompeg);
 }
 
 HanoiMoves::HanoiMoves() {
-		heightPerDisk = 10;
+
+		// dimensions of game
+		diskHeight = 10;
 		pegDistance = 70;
-		pegHeight = 30;
-		liftHeight = 100;
+		gameBaseHeight = 30;
+		diameterDifference = 10;
+		smallestDiskDiameter = 30;
+
+		// trajectory params
+		liftHeight = gameBaseHeight+50;
 		grippingDuration = 300;
 		grippingDurationBreak = 800;
+		gripperAddonToDisk = 20;
 
-		pegsBase[0] = Point(250, -pegDistance,pegHeight);
-		pegsBase[1] = Point(250, 0,pegHeight);
-		pegsBase[2] = Point(250, pegDistance,pegHeight);
+		pegsBase[0] = Point(250, -pegDistance,gameBaseHeight);
+		pegsBase[1] = Point(250, 0,gameBaseHeight);
+		pegsBase[2] = Point(250, pegDistance,gameBaseHeight);
 };
 
 void HanoiMoves::init(int numberOfDisks) {
 		numberOfDisksOnPeg[0] = numberOfDisks;
 		numberOfDisksOnPeg[1] = 0;
 		numberOfDisksOnPeg[2] = 0;
+
+		// empty the array that stores the disks per peg
+		for (int i = 0;i<MaxDisks; i++) {
+			diskNumbersPerPeg[0][i] = 0;
+			diskNumbersPerPeg[1][i] = 0;
+			diskNumbersPerPeg[2][i] = 0;
+		}
+
+		// on the left (0) peg we start with all the disks. Smallest Disk has the number 1
+		for (int i = numberOfDisks; i> 0;i--)
+			diskNumbersPerPeg[0][numberOfDisks-i] = i;
 }
 
 void HanoiMoves::addPose(Pose &pose, InterpolationType interpolationType, rational duration) {
@@ -69,31 +87,33 @@ void HanoiMoves::addPose(Pose &pose, InterpolationType interpolationType, ration
 
 	trajectory.insert(trajectory.end(), node);
 }
-void HanoiMoves::move(int diskNumber, int fromPegNumber, int toPegNumber) {
+void HanoiMoves::move(int fromPegNumber, int toPegNumber) {
 		Pose pose;
 
 		int fromPegDisks = numberOfDisksOnPeg[fromPegNumber];
 		int toPegDisks = numberOfDisksOnPeg[toPegNumber];
 
+		// which disk is to be moved?
+		int diskNumber = diskNumbersPerPeg[fromPegNumber][fromPegDisks-1];
+		int diskDiameter = smallestDiskDiameter + (diskNumber-1)* diameterDifference;
+
 		// move above the disk
 		pose.position= pegsBase[fromPegNumber];
 		pose.position.z += liftHeight;
 		pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance = 40;
+		pose.gripperDistance = diskDiameter + gripperAddonToDisk;
 		addPose(pose);
 
 		// go down
 		pose.position = pegsBase[fromPegNumber];
-		pose.position.z += heightPerDisk*fromPegDisks;
-		// pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance = 40;
+		pose.position.z += diskHeight*fromPegDisks;
 		addPose(pose,JOINT_LINEAR, grippingDuration);
 
 		// grab the disk
 		pose.position = pegsBase[fromPegNumber];
-		pose.position.z += heightPerDisk*fromPegDisks;
+		pose.position.z += diskHeight*fromPegDisks;
 		pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance = 0;
+		pose.gripperDistance = diskDiameter;
 		addPose(pose, JOINT_LINEAR,grippingDurationBreak);
 		addPose(pose);
 
@@ -101,40 +121,41 @@ void HanoiMoves::move(int diskNumber, int fromPegNumber, int toPegNumber) {
 		pose.position = pegsBase[fromPegNumber];
 		pose.position.z += liftHeight;
 		pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance = 0;
-		addPose(pose, POSE_CUBIC_BEZIER);
+		addPose(pose, POSE_LINEAR);
 
 		// go to the other peg
 		pose.position = pegsBase[toPegNumber];
 		pose.position.z += liftHeight;
 		pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance= 0;
 		addPose(pose);
 
 		// go down
 		pose.position = pegsBase[toPegNumber];
-		pose.position.z = pegHeight + heightPerDisk*toPegDisks;
+		pose.position.z += diskHeight*(toPegDisks+1);
 		pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance = 0;
 		addPose(pose, JOINT_LINEAR, grippingDuration);
 
 		// open gripper and let disk there
 		pose.position = pegsBase[toPegNumber];
-		pose.position.z = pegHeight + heightPerDisk*toPegDisks;
+		pose.position.z += diskHeight*(toPegDisks+1);
 		pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance = 40;
+		pose.gripperDistance = diskDiameter + gripperAddonToDisk;
+		addPose(pose, JOINT_LINEAR, grippingDurationBreak);
 		addPose(pose);
 
 		// go up
 		pose.position = pegsBase[toPegNumber];
-		pose.position.z = pegHeight + heightPerDisk*toPegDisks+50;
+		pose.position.z += liftHeight;
 		pose.orientation = Rotation(0,radians(90),0);
-		pose.gripperDistance = 40;
 		addPose(pose);
 
-		numberOfDisksOnPeg[fromPegNumber]--;
-		numberOfDisksOnPeg[toPegNumber]++;
+		// add disk to the other peg
+		diskNumbersPerPeg[toPegNumber][numberOfDisksOnPeg[toPegNumber]] = diskNumber;
+   		numberOfDisksOnPeg[toPegNumber]++;
 
+   		// remove disk from first peg
+		numberOfDisksOnPeg[fromPegNumber]--;
+		diskNumbersPerPeg[fromPegNumber][numberOfDisksOnPeg[fromPegNumber]] = 0;
 };
 
 HanoiMoves hanoi;
--- code/WalterPlanner/src/Hanoi.h
+++ code/WalterPlanner/src/Hanoi.h
@@ -19,7 +19,7 @@ public:
 	// Is called by hanoi-algorithm and needs to be redefined
 	// Represents one move of disk <diskNumber> from one peg to the other.
 	// Pegs are 0,2,1, disks are number beginning from 1, disks are moved from peg 0 to peg 1 (via peg 2)
-	virtual void move(int diskNumber, int fromPegNumber, int toPegNumber)  = 0;
+	virtual void move(int fromPegNumber, int toPegNumber)  = 0;
 	virtual void init(int noOfDisks) = 0;
 
 private:
@@ -31,19 +31,30 @@ public:
 	HanoiMoves();
 
 	virtual void init(int numberOfDisks);
-	virtual void move(int diskNumber, int fromPegNumber, int toPegNumber);
+	virtual void move(int fromPegNumber, int toPegNumber);
 
 	void addPose(Pose &pose, InterpolationType interpolationType = POSE_LINEAR, rational duration = 0.0);
 
-	Point pegsBase[3];
+	// dimensions of towers of hanoi
+	int gameBaseHeight;   			// height base of the games
+	int diskHeight;					// height of one disk
+	int pegDistance;				// distance of one peg to the next one
+	int smallestDiskDiameter;		// diameter of smallest disk
+	int diameterDifference;			// difference in diameter of a disk to its successor
 
-	int pegHeight;
-	int heightPerDisk;
-	int pegDistance;
-	int liftHeight;
-	int numberOfDisksOnPeg[3];
+
+	// trajectory
 	int grippingDuration;
 	int grippingDurationBreak;
+	int liftHeight;
+	int gripperAddonToDisk;
+
+	// temp. dimensions
+	int numberOfDisksOnPeg[3];
+#define MaxDisks 10
+	int diskNumbersPerPeg[3][MaxDisks];
+
+	Point pegsBase[3];
 
 };
 
